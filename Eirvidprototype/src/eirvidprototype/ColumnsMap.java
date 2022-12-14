30021eed0fc41987def8e02161ed69868416f67e
Amanda Lima Leite <Amandaleite@users.noreply.github.com>
2022-12-14 13:34:01 +0000

    First commit
    commit test

---
+++ Eirvidprototype/src/eirvidprototype/ColumnsMap.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eirvidprototype;
+
+/**
+ *
+ * @author Amanda Lima
+ * Student Number: 2020304
+ */
+public class ColumnsMap {
+    /* This class was created based on Algorithms, architecture design patterns classes
+    This create a BufferedReader, read the lines in the file into a Arraylist and return the lines
+    */
+    public MoviesRecord Map(String[] columns) {
+        String originalLanguage = columns[0];
+        String originalTitle = columns[1];
+        String overview = columns[2];
+        double popularity = Double.parseDouble(columns[3]);
+        //find how to get realease date
+        int runtime = Integer.parseInt(columns[5]);
+        String tagline = columns[6];
+        String title = columns[7];
+        double voteAverage = Double.parseDouble(columns[8]);
+        int voteCount = Integer.parseInt(columns[9]);
+        double price = Double.parseDouble(columns[10]);
+        
+        return new MoviesRecord(
+                originalLanguage,
+                originalTitle,
+                overview,
+                popularity,
+                runtime,
+                tagline,
+                title,
+                voteAverage,
+                voteCount,
+                price
+        );
+    
+}
