a1893275c78dc43f8b03a25856142b7c88c1c5b6
Pearle2020 <tchatchoualucresse@Gmail.com>
2022-12-20 08:55:00 +0000

    test

---
+++ Eirvidprototype/test/ConnectMySQL/registrationDataTest.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package ConnectMySQL;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author tchat
+ */
+public class registrationDataTest {
+    
+    public registrationDataTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of menu method, of class registrationData.
+     */
+    @Test
+    public void testMenu() {
+        System.out.println("menu");
+        registrationData.menu();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of logIn method, of class registrationData.
+     */
+    @Test
+    public void testLogIn() {
+        System.out.println("logIn");
+        registrationData.logIn();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of register method, of class registrationData.
+     */
+    @Test
+    public void testRegister() {
+        System.out.println("register");
+        registrationData.register();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
